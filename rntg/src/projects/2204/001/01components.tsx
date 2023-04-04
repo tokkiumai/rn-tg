4c9bf30184f5d321c6cc4392c3bc2a0ba2762526
yaki <kiwidancebad@gmail.com>
2023-04-04 15:11:38 +0300

    add components for flutter-like animation

--- rntg/src/projects/2204/001/01components.tsx
+++ rntg/src/projects/2204/001/01components.tsx
@@ -1,9 +1,15 @@
 import type { Section } from './01constants'
-import { CURSOR_WIDTH } from './01constants'
+import {
+  CURSOR_WIDTH,
+  MEDIUM_HEADER_HEIGHT,
+  PADDING,
+  SMALL_HEADER_HEIGHT,
+} from './01constants'
 import React from 'react'
-import { Dimensions, Image, StyleSheet, View } from 'react-native'
+import { Dimensions, Image, Platform, StyleSheet, View } from 'react-native'
+import Animated from 'react-native-reanimated'
 
-let { width: dWidth } = Dimensions.get('window')
+let { width: dWidth, height: dHeight } = Dimensions.get('window')
 
 let cStyles = StyleSheet.create({
   root: {
@@ -50,5 +56,60 @@ export function Header({ section }: HeaderProps) {
     <View style={hStyles.root}>
       <Image source={section.image} style={hStyles.image} />
     </View>
+  )
+}
+
+let { Value, Extrapolate, interpolateNode } = Animated
+
+interface LabelProps {
+  section: Section
+  x: typeof Value
+  y: typeof Value
+  index: number
+}
+
+let charWidth = Platform.OS === 'ios' ? 19.3 : 19
+let fontSize = 32
+let fontFamily = Platform.OS === 'ios' ? 'Menlo' : 'monospace'
+
+let lStyles = StyleSheet.create({
+  root: {
+    ...StyleSheet.absoluteFillObject,
+    padding: PADDING,
+    justifyContent: 'center',
+  },
+  label: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize,
+    fontFamily,
+  },
+})
+
+export function Label(props: LabelProps) {
+  let { x, y, index, section } = props
+  let opacity = interpolateNode(x, {
+    inputRange:
+      index === 0
+        ? [0, 0, dWidth]
+        : [dWidth * (index - 1), dWidth * index, dWidth * (index + 1)],
+    outputRange: [0.5, 1, 0, 5],
+    extrapolate: Extrapolate.CLAMP,
+  })
+  let labelWidth = interpolateNode(y, {
+    inputRange: [
+      0,
+      dHeight - MEDIUM_HEADER_HEIGHT,
+      dHeight - SMALL_HEADER_HEIGHT,
+    ],
+    outputRange: [section.title.length * charWidth, dWidth, dWidth],
+    extrapolate: Extrapolate.CLAMP,
+  })
+  return (
+    <Animated.View style={[lStyles.root, { opacity }]}>
+      <Animated.Text style={[lStyles.label, { width: labelWidth }]}>
+        {section.title.toUpperCase()}
+      </Animated.Text>
+    </Animated.View>
   )
 }
