a833bb55f421d1e4e608d75261f229afcf2d7f49
yaki <kiwidancebad@gmail.com>
2023-04-08 14:32:13 +0300

    add Riveo

--- rntg/App.tsx
+++ rntg/App.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { useColorScheme, View } from 'react-native'
 
 import { Colors } from 'react-native/Libraries/NewAppScreen'
+import Riveo from './src/projects/riveo/Riveo'
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   }
 
-  return <View style={backgroundStyle}></View>
+  return (
+    <View style={backgroundStyle}>
+      <Riveo />
+    </View>
+  )
 }
 
 export default App
