f7140877037a417d08ec3d75a2fa6f927d2f6c44
yaki <kiwidancebad@gmail.com>
2023-04-11 11:59:05 +0300

    add module resolver

--- rntg/.eslintrc.js
+++ rntg/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
   rules: {
     semi: [2, 'never'],
     'prettier/prettier': 0,
--- rntg/babel.config.js
+++ rntg/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          root: ['./'],
+          projects: './src/projects',
+        },
+      },
+    ],
+  ],
+}
